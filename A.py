7ba33a90de5b09f02cbf4b27470b949745d1d931
rohan17088 <44478017+rohan17088@users.noreply.github.com>
2020-03-22 16:14:03 +0530

    Update A.py

--- A.py
+++ A.py
@@ -2,6 +2,13 @@ import pickle
 import methods
 import socket
 import datetime
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import ast
+import hashlib
+import random
+import datetime
 
 ID = -1
 key = -1
@@ -21,7 +28,7 @@ listOfID['B'] = B_ID
 PrivateKey = key
 PublicKey = key.publickey()
 
-port1 = 4997
+port1 = 4996
 port2 = 7000
 
 #####################################################################################
@@ -37,7 +44,6 @@ for line in f:
 
 hashed_file = methods.hash_string(file)
 message = hashed_file + '||' + str(listOfID['B'])
-print (message)
 
 s = socket.socket()
 port = port1  
@@ -46,8 +52,26 @@ s.connect(('127.0.0.1', port))
 print ('From A to TSA')
 s.send(str.encode(message))
 
-new_message = s.recv(1024)
-print(new_message)
+new_message = s.recv(2048)
+
+splitted_message = new_message.split(str.encode("||"))
+encrypted_hash = splitted_message[0]
+hashed_file = splitted_message[1]
+time = splitted_message[4]
+expiry = splitted_message[5]
+B_key = splitted_message[6]
+
+B_key = RSA.importKey(B_key)
+TSA_key = splitted_message[7]
+TSA_key = RSA.importKey(TSA_key)
+
+decrypted_hash = methods.decrypt(encrypted_hash, TSA_key)
+
+
+
+
+
+
 
 s.close()
 
