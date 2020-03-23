7a5718d8ab745b16530c4f9d94fece62e45898df
rohan17088 <44478017+rohan17088@users.noreply.github.com>
2020-03-23 15:55:18 +0530

    Update A.py

--- A.py
+++ A.py
@@ -19,17 +19,17 @@ listOfID = {}
 with open('initialsetup.pkl', 'rb') as input:
 	pickle.load(input)
 	pickle.load(input)
-	ID = pickle.load(input) 
+	ID = int(pickle.load(input))
 	key = pickle.load(input)
-	B_ID = pickle.load(input) 
+	B_ID = int(pickle.load(input))
 	pickle.load(input) 
 
 listOfID['B'] = B_ID
-PrivateKey = key
-PublicKey = key.publickey()
+PrivateKey = key[0]
+PublicKey = key[1]
 
-port1 = 4996
-port2 = 7000
+port1 = 5003
+port2 = 7005
 
 #####################################################################################
 
@@ -45,35 +45,66 @@ for line in f:
 hashed_file = methods.hash_string(file)
 message = hashed_file + '||' + str(listOfID['B'])
 
-s = socket.socket()
-port = port1  
-s.connect(('127.0.0.1', port))
+time = -1
+expiry = -1
+new_message = -1
 
-print ('From A to TSA')
-s.send(str.encode(message))
+if time == -1:
+	while True:
+		s = socket.socket()
+		port = port1  
+		s.connect(('127.0.0.1', port))
 
-new_message = s.recv(2048)
+		print ('From A to TSA')
+		s.send(str.encode(message))
 
-splitted_message = new_message.split(str.encode("||"))
-encrypted_hash = splitted_message[0]
-hashed_file = splitted_message[1]
-time = splitted_message[4]
-expiry = splitted_message[5]
-B_key = splitted_message[6]
+		new_message = s.recv(2048)
+		new_message = new_message.decode()
 
-B_key = RSA.importKey(B_key)
-TSA_key = splitted_message[7]
-TSA_key = RSA.importKey(TSA_key)
+		splitted_message = new_message.split("||")
 
-decrypted_hash = methods.decrypt(encrypted_hash, TSA_key)
+		encrypted_hash = splitted_message[0]
+		hashed_file = splitted_message[1]
+		time = splitted_message[4]
+		expiry = splitted_message[5]
+		B_Public_Key = int(splitted_message[6])
+		TSA_key = int(splitted_message[7])
+
+		message_to_be_checked = ""
+		for i in range(1, 7):
+			message_to_be_checked += splitted_message[i] + "||"
+
+		message_to_be_checked += splitted_message[7]
+
+
+		decrypted_hash = methods.decrypt(encrypted_hash, TSA_key)
+		s.close()
+
+
+		if decrypted_hash==methods.hash_string(message_to_be_checked):
+			break
+
+print("OK")
+
+
+# # decrypted_hash = decrypted_hash.decode()
+# # print(type(encrypted_hash))
+# # encrypted_hash = encrypted_hash.decode()
+# decrypted_hash = methods.decrypt(encrypted_hash, TSA_key)
+# print(decrypted_hash)
+# print(type(decrypted_hash))
+
+# hash_to_be_checked = ""
+
+# for i in range(1,8):
+# 	hash_to_be_checked += splitted_message[i].decode()
+
+# print(decrypted_hash==methods.hash_string(hash_to_be_checked))
 
 
 
 
 
-
-
-s.close()
 
 
 
@@ -83,10 +114,17 @@ s.close()
 Communication between A and B
 '''
 
-# s = socket.socket()
-# port = port2  
-# s.connect(('127.0.0.1', port))
+s = socket.socket()
+port = port2 
+s.connect(('127.0.0.1', port))
 # time_now = datetime.datetime.now()
+encrypted_file = methods.encrypt(file, B_Public_Key)
+message = encrypted_file + "||" + new_message
 # message = 'from A to B' + '||' + str(time_now)
-# s.send(str.encode(message))
-# s.close() 
+s.send(str.encode(message))
+s.close()
+
+
+
+
+
