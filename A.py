be15d0af518a403f0e13481fa12b331482541cbd
rohan17088 <44478017+rohan17088@users.noreply.github.com>
2020-03-23 17:34:02 +0530

    Update A.py

--- A.py
+++ A.py
@@ -28,8 +28,8 @@ listOfID['B'] = B_ID
 PrivateKey = key[0]
 PublicKey = key[1]
 
-port1 = 5003
-port2 = 7005
+port1 = 5009
+port2 = 7008
 
 #####################################################################################
 
@@ -45,9 +45,22 @@ for line in f:
 hashed_file = methods.hash_string(file)
 message = hashed_file + '||' + str(listOfID['B'])
 
-time = -1
+
+
+
+time = 0
 expiry = -1
-new_message = -1
+message_from_tsa = -1
+
+try:
+	with open('certificate.pkl', 'rb') as input:
+		message_from_tsa = pickle.load(input)
+		splitted_message = message_from_tsa.split("||")
+		expiry = splitted_message[5]
+		if datetime.datetime.strptime(expiry, '%Y-%m-%d %H:%M:%S.%f') < datetime.datetime.now():
+			time = -1
+except:
+	time = -1
 
 if time == -1:
 	while True:
@@ -58,10 +71,13 @@ if time == -1:
 		print ('From A to TSA')
 		s.send(str.encode(message))
 
-		new_message = s.recv(2048)
-		new_message = new_message.decode()
+		message_from_tsa = s.recv(2048)
+		message_from_tsa = message_from_tsa.decode()
 
-		splitted_message = new_message.split("||")
+		with open('certificate.pkl', 'wb') as output:
+			pickle.dump(message_from_tsa, output, pickle.HIGHEST_PROTOCOL)
+
+		splitted_message = message_from_tsa.split("||")
 
 		encrypted_hash = splitted_message[0]
 		hashed_file = splitted_message[1]
@@ -84,7 +100,25 @@ if time == -1:
 		if decrypted_hash==methods.hash_string(message_to_be_checked):
 			break
 
-print("OK")
+else:
+	splitted_message = message_from_tsa.split("||")
+	encrypted_hash = splitted_message[0]
+	hashed_file = splitted_message[1]
+	time = splitted_message[4]
+	expiry = splitted_message[5]
+	B_Public_Key = int(splitted_message[6])
+	TSA_key = int(splitted_message[7])
+
+	message_to_be_checked = ""
+	for i in range(1, 7):
+		message_to_be_checked += splitted_message[i] + "||"
+	message_to_be_checked += splitted_message[7]
+	decrypted_hash = methods.decrypt(encrypted_hash, TSA_key)
+
+	if decrypted_hash != methods.hash_string(message_to_be_checked):
+		print("Integrity Failed")
+		exit()
+
 
 
 # # decrypted_hash = decrypted_hash.decode()
@@ -119,7 +153,7 @@ port = port2
 s.connect(('127.0.0.1', port))
 # time_now = datetime.datetime.now()
 encrypted_file = methods.encrypt(file, B_Public_Key)
-message = encrypted_file + "||" + new_message
+message = encrypted_file + "||" + message_from_tsa
 # message = 'from A to B' + '||' + str(time_now)
 s.send(str.encode(message))
 s.close()
